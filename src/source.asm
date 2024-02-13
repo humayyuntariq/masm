4c01f28fc6a0e92b898ea29a129f86e2470fbdc6
Hamza Ihsan <85873694+thehamzaihsan@users.noreply.github.com>
2024-02-13 10:59:34 +0000

    change

--- src/source.asm
+++ src/source.asm
@@ -1,19 +1,26 @@
-INCLUDE irvine32.inc
-.data 
-array word 10,20,30,40
+TITLE Hello World
+
+; Name: 
+; Date: 
+; ID: 
+; Description: 
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
+
+; these two lines are only necessary if you're not using Visual Studio
+INCLUDELIB kernel32.lib
+INCLUDELIB user32.lib
+
+.data
+    helloMsg BYTE "Hello, World!", 0
+
 .code
-main proc
-mov eax,0
-mov esi, offset array
-add ax, [esi] 
-add esi,2
-add ax,[esi]
-add esi,2
-add ax, [esi] 
-add esi,2
-add ax, [esi] 
-add esi,2
-call writeint
-exit
-main endp
-end main
+main PROC
+    mov edx, OFFSET helloMsg ; Load the address of helloMsg into edx
+    call WriteString         ; Call Irvine's WriteString procedure to display the string
+    call DumpRegs            ; displays registers in console
+    exit                     ; Exit the program
+main ENDP
+
+END main
