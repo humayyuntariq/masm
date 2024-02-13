19b0ddd5a3e896a28d33408758b45d145b4b86dd
Hamza Ihsan <85873694+thehamzaihsan@users.noreply.github.com>
2024-02-13 10:54:35 +0000

    code

--- src/source.asm
+++ src/source.asm
@@ -1,28 +1,19 @@
-TITLE
-
-; Name: 
-; Date: 
-; ID: 
-; Description: 
-
-INCLUDE Irvine32.inc
-INCLUDELIB Irvine32.lib
-
-; these two lines are only necessary if you're not using Visual Studio
-INCLUDELIB kernel32.lib
-INCLUDELIB user32.lib
-
-.data
-    
-	; data declarations go here
-
+INCLUDE irvine32.inc
+.data 
+array word 10,20,30,40
 .code
-main PROC
-	
-	; code goes here
-	call DumpRegs ; displays registers in console
-
-	exit
-
-main ENDP
-END main
+main proc
+mov eax,0
+mov esi, offset array
+add ax, [esi] 
+add esi,2
+add ax,[esi]
+add esi,2
+add ax, [esi] 
+add esi,2
+add ax, [esi] 
+add esi,2
+call writeint
+exit
+main endp
+end main
